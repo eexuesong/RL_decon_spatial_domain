17fcb632c52834885d06e04b4d84eb5b1c82d100
Xuesong Li <107074600+eexuesong@users.noreply.github.com>
2023-05-11 15:13:02 -0400

    Add files via upload

--- iSIM_decon_ui.m
+++ iSIM_decon_ui.m
@@ -7,43 +7,47 @@
 % set parameters
 inputDialog = uifigure('Name', 'Set Parameters', 'Position', [300 500 500 320]);
 
+% panels
+pnl_60x = uipanel(inputDialog, 'Position', [10 210 480 105]);
+pnl_100x = uipanel(inputDialog, 'Position', [10 100 480 105]);
+
 % set FWHM of PSFs (60X)
-uilabel(inputDialog, 'Text', 'UPLSAPO60XS2', 'Position', [20 290 100 20]);
-uilabel(inputDialog, 'Text', '405 nm:', 'Position', [100 270 60 20]);
-uilabel(inputDialog, 'Text', '488 nm:', 'Position', [200 270 60 20]);
-uilabel(inputDialog, 'Text', '561 nm:', 'Position', [300 270 60 20]);
-uilabel(inputDialog, 'Text', '633 nm:', 'Position', [400 270 60 20]);
+uilabel(pnl_60x, 'Text', 'UPLSAPO60XS2', 'Position', [10 80 100 20]);
+uilabel(pnl_60x, 'Text', '405 nm:', 'Position', [90 60 60 20]);
+uilabel(pnl_60x, 'Text', '488 nm:', 'Position', [190 60 60 20]);
+uilabel(pnl_60x, 'Text', '561 nm:', 'Position', [290 60 60 20]);
+uilabel(pnl_60x, 'Text', '633 nm:', 'Position', [390 60 60 20]);
 
-uilabel(inputDialog, 'Text', 'XY (nm):', 'Position', [40 240 60 20]);
-txa_60x_FWHM_xy{1} = uitextarea(inputDialog, 'Value', 'NA', 'Position', [100 250 60 20]);
-txa_60x_FWHM_xy{2} = uitextarea(inputDialog, 'Value', 'NA', 'Position', [200 250 60 20]);
-txa_60x_FWHM_xy{3} = uitextarea(inputDialog, 'Value', 'NA', 'Position', [300 250 60 20]);
-txa_60x_FWHM_xy{4} = uitextarea(inputDialog, 'Value', 'NA', 'Position', [400 250 60 20]);
+uilabel(pnl_60x, 'Text', 'XY (nm):', 'Position', [30 40 60 20]);
+txa_60x_FWHM_xy{1} = uitextarea(pnl_60x, 'Value', 'NA', 'Position', [90 40 60 20]);
+txa_60x_FWHM_xy{2} = uitextarea(pnl_60x, 'Value', 'NA', 'Position', [190 40 60 20]);
+txa_60x_FWHM_xy{3} = uitextarea(pnl_60x, 'Value', 'NA', 'Position', [290 40 60 20]);
+txa_60x_FWHM_xy{4} = uitextarea(pnl_60x, 'Value', 'NA', 'Position', [390 40 60 20]);
 
-uilabel(inputDialog, 'Text', 'Z (nm):', 'Position', [40 220 60 20]);
-txa_60x_FWHM_z{1} = uitextarea(inputDialog, 'Value', 'NA', 'Position', [100 220 60 20]);
-txa_60x_FWHM_z{2} = uitextarea(inputDialog, 'Value', 'NA', 'Position', [200 220 60 20]);
-txa_60x_FWHM_z{3} = uitextarea(inputDialog, 'Value', 'NA', 'Position', [300 220 60 20]);
-txa_60x_FWHM_z{4} = uitextarea(inputDialog, 'Value', 'NA', 'Position', [400 220 60 20]);
+uilabel(pnl_60x, 'Text', 'Z (nm):', 'Position', [38 10 60 20]);
+txa_60x_FWHM_z{1} = uitextarea(pnl_60x, 'Value', 'NA', 'Position', [90 10 60 20]);
+txa_60x_FWHM_z{2} = uitextarea(pnl_60x, 'Value', 'NA', 'Position', [190 10 60 20]);
+txa_60x_FWHM_z{3} = uitextarea(pnl_60x, 'Value', 'NA', 'Position', [290 10 60 20]);
+txa_60x_FWHM_z{4} = uitextarea(pnl_60x, 'Value', 'NA', 'Position', [390 10 60 20]);
 
 % set FWHM of PSFs (100X)
-uilabel(inputDialog, 'Text', 'UPLSAPO100XO', 'Position', [20 180 100 20]);
-uilabel(inputDialog, 'Text', '405 nm:', 'Position', [100 160 60 20]);
-uilabel(inputDialog, 'Text', '488 nm:', 'Position', [200 160 60 20]);
-uilabel(inputDialog, 'Text', '561 nm:', 'Position', [300 160 60 20]);
-uilabel(inputDialog, 'Text', '633 nm:', 'Position', [400 160 60 20]);
+uilabel(pnl_100x, 'Text', 'UPLSAPO100XO', 'Position', [10 80 100 20]);
+uilabel(pnl_100x, 'Text', '405 nm:', 'Position', [90 60 60 20]);
+uilabel(pnl_100x, 'Text', '488 nm:', 'Position', [190 60 60 20]);
+uilabel(pnl_100x, 'Text', '561 nm:', 'Position', [290 60 60 20]);
+uilabel(pnl_100x, 'Text', '633 nm:', 'Position', [390 60 60 20]);
 
-uilabel(inputDialog, 'Text', 'XY (nm):', 'Position', [40 140 60 20]);
-txa_100x_FWHM_xy{1} = uitextarea(inputDialog, 'Value', '155', 'Position', [100 140 60 20]);
-txa_100x_FWHM_xy{2} = uitextarea(inputDialog, 'Value', '184', 'Position', [200 140 60 20]);
-txa_100x_FWHM_xy{3} = uitextarea(inputDialog, 'Value', '207', 'Position', [300 140 60 20]);
-txa_100x_FWHM_xy{4} = uitextarea(inputDialog, 'Value', '240', 'Position', [400 140 60 20]);
+uilabel(pnl_100x, 'Text', 'XY (nm):', 'Position', [30 40 60 20]);
+txa_100x_FWHM_xy{1} = uitextarea(pnl_100x, 'Value', '155', 'Position', [90 40 60 20]);
+txa_100x_FWHM_xy{2} = uitextarea(pnl_100x, 'Value', '184', 'Position', [190 40 60 20]);
+txa_100x_FWHM_xy{3} = uitextarea(pnl_100x, 'Value', '207', 'Position', [290 40 60 20]);
+txa_100x_FWHM_xy{4} = uitextarea(pnl_100x, 'Value', '240', 'Position', [390 40 60 20]);
 
-uilabel(inputDialog, 'Text', 'Z (nm):', 'Position', [40 110 60 20]);
-txa_100x_FWHM_z{1} = uitextarea(inputDialog, 'Value', '350', 'Position', [100 110 60 20]);
-txa_100x_FWHM_z{2} = uitextarea(inputDialog, 'Value', '400', 'Position', [200 110 60 20]);
-txa_100x_FWHM_z{3} = uitextarea(inputDialog, 'Value', '450', 'Position', [300 110 60 20]);
-txa_100x_FWHM_z{4} = uitextarea(inputDialog, 'Value', '533', 'Position', [400 110 60 20]);
+uilabel(pnl_100x, 'Text', 'Z (nm):', 'Position', [38 10 60 20]);
+txa_100x_FWHM_z{1} = uitextarea(pnl_100x, 'Value', '350', 'Position', [90 10 60 20]);
+txa_100x_FWHM_z{2} = uitextarea(pnl_100x, 'Value', '400', 'Position', [190 10 60 20]);
+txa_100x_FWHM_z{3} = uitextarea(pnl_100x, 'Value', '450', 'Position', [290 10 60 20]);
+txa_100x_FWHM_z{4} = uitextarea(pnl_100x, 'Value', '533', 'Position', [390 10 60 20]);
 
 % set iteration number
 uilabel(inputDialog, 'Text', 'Iter Num:', 'Position', [40 60 60 20]);
@@ -56,7 +60,7 @@ txa_itNum{4} = uitextarea(inputDialog, 'Value', '10', 'Position', [400 60 60 20]
 pb1 = uibutton(inputDialog, 'push', 'Position', [180 20 50 20], 'Text', 'Yes',...
     'ButtonPushedFcn', @(pb1, event) pbYes(inputDialog, txa_60x_FWHM_xy, txa_60x_FWHM_z, txa_100x_FWHM_xy, txa_100x_FWHM_z, txa_itNum, path_data));
 pb2 = uibutton(inputDialog, 'push', 'Position', [280 20 50 20], 'Text', 'Cancel',...
-    'ButtonPushedFcn', @(pb1,event) pbCancel(inputDialog));
+    'ButtonPushedFcn', @(pb1, event) pbCancel(inputDialog));
 
 %% Cancel dialog box
 function pbCancel(inputDialog)
